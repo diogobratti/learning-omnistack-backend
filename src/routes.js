856672121092911029900fb92aa3b8859c960bf7
Diogo Bratti <dbratti.programador@gmail.com>
2019-05-09 11:55:01 -0300

    First Controller created.

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const routes = express.Router();
 
+const BoxController = require('./controllers/BoxController');
+
 routes.get('/teste', (req, res) => {
   return res.send('Hello World333aa');
-})
+});
+
+
+routes.post('/boxes', BoxController.store);
+
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express(); 
+
+mongoose.connect("mongodb+srv://dbratti_learning:foryoureyesonly@cluster0-r2zm7.mongodb.net/test?retryWrites=true",
+{
+  useNewUrlParser: true
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
